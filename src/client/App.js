34e1e4fd2504f507165646e8f385e18c31f94d79
Philippe Lamarre <philippe.lamarre@radio-canada.ca>
2018-12-11 21:26:55 -0500

    ajout du react router

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './app.scss';
 import ReactImage from './img/react.png';
+
 
 export default class App extends Component {
   state = { username: null };
